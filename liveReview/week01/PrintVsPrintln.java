f0607d40582f57f80ea3b47309eb9e845bf1d9ec
OscarSah <oscar@cybertekschool.com>
2022-01-15 19:23:53 +0300

    Fist Task of Review finished

--- liveReview/week01/PrintVsPrintln.java
+++ liveReview/week01/PrintVsPrintln.java
@@ -1,4 +1,14 @@
 package week01;
 
 public class PrintVsPrintln {
+
+    public static void main(String[] args) {
+
+        System.out.print("One"); // CTRL + D , CMND+D
+        System.out.println("Two"); // print first then go to the next line
+        System.out.print("Three");
+
+        System.err.println("Error Statement");
+
+    }
 }
