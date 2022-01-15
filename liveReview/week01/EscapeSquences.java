072a05cdb00f3335080101cd33c0bde980371b46
OscarSah <oscar@cybertekschool.com>
2022-01-15 20:23:49 +0300

    Third Class Example

---
+++ liveReview/week01/EscapeSquences.java
@@ -0,0 +1,20 @@
+package week01;
+
+public class EscapeSquences {
+
+    public static void main(String[] args) {
+
+        /*
+        \t : tab
+        \n: takes it to next line
+        \\ : prints \
+        \" : prints "
+         */
+
+        System.out.println("Learn\tJava\n\tthe\nHard\tWay\n\n"); //   \H : illegal escape squence
+
+        System.out.println("\tLearn JAVA the \" EASY WAY \" Way! \n\t\t With Oscar \\\\");
+
+
+    }
+}
