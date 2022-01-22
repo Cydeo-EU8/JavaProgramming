816f9f51274f4d85d84cec32db1716e678a9641f
OscarSah <oscar@cybertekschool.com>
2022-01-22 18:17:28 +0300

    Naming Convention Examples

---
+++ liveReview/week02/Naming_Convention.java
@@ -0,0 +1,22 @@
+package week02;
+
+public class Naming_Convention {
+    public static void main(String[] args) {
+        /* Valid : V  Invalid: I
+ _average : V
+ B4 : V
+ ABC : V
+ println : Valid
+ "hello" : I - starts with "
+ for : I -- reserved words
+ sum_of_data : V
+ first-name : I   - dash sign not accepted
+ AnnualSalary: V
+ 42isThesolution : I - starts with number
+         */
+
+        int _average;
+        String println = "Hello";
+
+    }
+}
