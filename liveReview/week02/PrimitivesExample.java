0d61466d8bb6a38ca714223b92467ba04716ca6a
OscarSah <oscar@cybertekschool.com>
2022-01-22 18:44:34 +0300

    Data Types

---
+++ liveReview/week02/PrimitivesExample.java
@@ -0,0 +1,30 @@
+package week02;
+
+public class PrimitivesExample {
+    public static void main(String[] args) {
+
+/*
+        DataType variableName = Value; Declare and initialize at the same line
+
+        DataType variableName;
+        variableName = value;
+
+        DataType variableNameOne, variableNameTwo;
+ */
+
+        byte num1; // Declaring a variable
+        num1 = 123; // Initializing a variable , assigning a value
+        num1 = 121; // re-assign a value
+        short num2 = 12398; // Declare and initialize at the same line
+        int distance = 1_000_000_000;
+        long distanceMore = 3_000_000_000l; // we have to put L to define the variable
+        long distanceShorter = 1_000_000_000; // if it does not pass the limit then we don't have to give
+
+        float rate = 1000.5F;
+        double dNumber = 123.6;
+
+        System.out.println("----Castings-----");
+
+    }
+
+}
