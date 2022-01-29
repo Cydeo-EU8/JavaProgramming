74b7fd1ba22234d224bf6df804f5109d934bf5e3
OscarSah <oscar@cybertekschool.com>
2022-01-29 19:27:30 +0300

    Third Week Fifth Example Relational

---
+++ liveReview/week03/RelationalOperatorExample.java
@@ -0,0 +1,18 @@
+package week03;
+
+public class RelationalOperatorExample {
+    public static void main(String[] args) {
+
+
+        int i1,i2;
+        i1=100;
+        i2 =20;
+
+     //   i1>i2  this will return boolean data type
+
+        boolean b = i1>i2;
+
+        System.out.println(" Is the first number bigger than second one " + b);
+
+    }
+}
