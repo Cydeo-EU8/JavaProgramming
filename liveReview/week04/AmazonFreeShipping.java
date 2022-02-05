d8291cd4f7a0bf094815b725e530d4236747c7f9
OscarSah <oscar@cybertekschool.com>
2022-02-05 22:23:24 +0300

    Week Four First Example

---
+++ liveReview/week04/AmazonFreeShipping.java
@@ -0,0 +1,18 @@
+package week04;
+
+public class AmazonFreeShipping {
+    public static void main(String[] args) {
+
+        double totalPrice = 55.50;
+
+        if(totalPrice >= 25.0 ){
+            System.out.println("FREE Shipping eligible. Your order total : $ " + totalPrice);
+        } else {
+            System.out.println("NOT Eligible for free shipping : $ " + totalPrice);
+        }
+
+        System.out.println("Thanks for shopping with us !");
+
+
+    }
+}
