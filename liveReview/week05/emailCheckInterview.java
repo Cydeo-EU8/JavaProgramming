e4fbf89bb1c663c3e34d6544994bf7dd6f13a3a6
OscarSah <oscar@cybertekschool.com>
2022-02-12 20:49:38 +0300

    Task Shared

---
+++ liveReview/week05/emailCheckInterview.java
@@ -0,0 +1,18 @@
+package week05;
+
+public class emailCheckInterview {
+   /*
+Your team has created a new bank website that requires email address to be validated.
+The email string must contain an '@' character.
+The email string must contain an '.' character.
+The '@' must contain at least one character in front of it.
+e.g. "a@example.com" is valid while "@example.com" is invalid.
+The '.' and '@' must be in the appropriate places.
+e.g. "mike.smith@com" is invalid while "mike.smith@example.com" is valid.
+For a given string, find a solution that writes true on the console if an email is valid and false if it is invalid.
+Examples:
+str = "test@example.com" --> true
+str = "test@example.co.in --> true
+str = "@example.com" --> false
+ */
+}
