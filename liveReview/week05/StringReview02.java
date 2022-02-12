7ca0fbcbce487eae7c95e361cfcf2886cf645d69
OscarSah <oscar@cybertekschool.com>
2022-02-12 19:25:41 +0300

    Example Two Contd.

--- liveReview/week05/StringReview02.java
+++ liveReview/week05/StringReview02.java
@@ -4,6 +4,14 @@ public class StringReview02 {
     public static void main(String[] args) {
         /*
         length(): it does not take parameter, it returns an integer number
+        replace()
+        toUpper or Lower case
+        substring
+        charAt
+        indexof()
+        lastindexof()
+        equals()
+        trim()
          */
 
         String message = "EU8 is Awesome!!";
@@ -13,7 +21,31 @@ public class StringReview02 {
         if(message.length()==16){
             System.out.println("happy testing");
         }
+        System.out.println(message.toUpperCase());
+        System.out.println("message after uppercase");
+        System.out.println(message);
 
+        // assign changes to a reference
+        String messageTwo = message.toUpperCase();
+        System.out.println(messageTwo); // EU8 IS AWESOME!!
 
+        String str = " abc ";
+        str = str.trim();
+        System.out.println(str);
+
+        //  can we also trim not the spaces before or after, but the one that is inside the text?
+        String str2 = " abc cde ";
+        str2 = str2.replace(" ",""); // abccde
+        System.out.println(str2);
+
+        String str3 = " EU8 Awesome ";
+        str3 = str3.trim().replaceFirst(" ",""); // EU8Awesome
+        System.out.println(str3);
+
+        // can we trim and replace and do upperCase and substring in the same expression  ??
+        // string methods chaining: possible YES
+        String str4 = " EU8 Awesome ";
+        str4 = str4.trim().replaceFirst(" ","").toUpperCase().substring(3); // AWESOME
+        System.out.println(str4);
     }
 }
