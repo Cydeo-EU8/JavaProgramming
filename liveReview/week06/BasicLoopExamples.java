f864d76b1a9021716ac8a26705cdf29537185eee
OscarSah <oscar@cybertekschool.com>
2022-02-19 22:33:57 +0300

    Example Three

--- liveReview/week06/BasicLoopExamples.java
+++ liveReview/week06/BasicLoopExamples.java
@@ -61,8 +61,28 @@ public class BasicLoopExamples {
             }
         }
         System.out.println("Count Of Even numbers between 5 and 50 = " + countOfEven);
+        /**
+         *
+         * Question-4:
 
+         Print the table of 12 using for loop.
+         Output should be in following format:
+         12 X 1 = 12
+         12 X 2 = 24
+         12 X 3 = 36
+         12 X 4 = 48
+         12 X 5 = 60
+         12 X 6 = 72
+         12 X 7 = 84
+         12 X 8 = 96
+         12 X 9 = 108
+         12 X 10 = 120
+         */
 
+        int j = 1;
+        for( ; j<=10 ; j++ ){
+            System.out.println("12 X "+j+" = "+(12*j));
+        }
 
 
     }
