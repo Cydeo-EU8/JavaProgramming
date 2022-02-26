faee97a6f36816262df9b86a7f903696f01f7e96
OscarSah <oscar@cybertekschool.com>
2022-02-26 23:40:19 +0300

    Task 02

--- liveReview/week07/Task02.java
+++ liveReview/week07/Task02.java
@@ -19,5 +19,25 @@ public class Task02 {
          */
 
 
+        for (int i = 1; i <= 6; i++) {  // handles rows
+
+            for (int j = 1; j <= i; j++) {   // handles columns
+
+                // ternary operator
+
+              // System.out.print(j%2==1? 1 : 0 ); // for odd columns print "1", for even columns "0"
+
+                if(j%2==1){
+                    System.out.print(1);
+                }else{
+                    System.out.print(0);
+                }
+
+            }
+            System.out.println();
+
+        }
+
+
     }
 }
