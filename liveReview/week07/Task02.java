e94f2f3be6dad9602309dfcf52facea8a312ae84
OscarSah <oscar@cybertekschool.com>
2022-02-26 23:31:48 +0300

    Task 02

---
+++ liveReview/week07/Task02.java
@@ -0,0 +1,23 @@
+package week07;
+
+public class Task02 {
+    public static void main(String[] args) {
+
+        /*
+        	Write a Java program to print the following pattern
+
+		1
+		10
+		101
+		1010
+		10101
+		101010
+
+		again we have 6 rows , 6 columns
+		patern: for odd columns print "1", for even columns "0"
+
+         */
+
+
+    }
+}
