feeebcca9a47757ced07b75dcc7a300cb2cffae3
OscarSah <oscar@cybertekschool.com>
2022-02-26 23:08:26 +0300

    week 07 review third example

---
+++ liveReview/week07/Task01.java
@@ -0,0 +1,16 @@
+package week07;
+
+public class Task01 {
+    public static void main(String[] args) {
+/*
+* Print below pattern using loop
+
+			1
+			2 6
+			3 7 10
+			4 8 11 13
+			5 9 12 14 15
+ */
+
+    }
+}
