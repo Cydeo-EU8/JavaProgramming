fe409ab2e9c11656a7b0ba1fd2725c576ac1dc06
OscarSah <oscar@cybertekschool.com>
2022-02-26 22:19:38 +0300

    week 07 review 1 task

--- liveReview/week07/WhileHungary.java
+++ liveReview/week07/WhileHungary.java
@@ -2,9 +2,10 @@ package week07;
 
 public class WhileHungary {
     public static void main(String[] args) {
-        boolean isHungary = true;
+  //      boolean isHungary = true;
         int bananas = 0;
         int countToFull = 3;
+  /*
         while(isHungary){
             bananas++;
             System.out.println("Eating a banana: \uD83C\uDF4C "+bananas);
@@ -15,7 +16,17 @@ public class WhileHungary {
                 isHungary = false;
             }
         }
+*/
 
+        while (true){
+            // condition that will take us out of the loop
+            if(bananas==countToFull){
+                break;  // branching statement
+            }
+            bananas++;
+            System.out.println("Eating a banana: \uD83C\uDF4C "+bananas);
+
+        }
         System.out.println("Had enough bananas, let's get back to study");
 
     }
