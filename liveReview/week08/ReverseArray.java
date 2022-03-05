543115d20e9984fc849faf4b00ef86fc41747b67
OscarSah <oscar@cybertekschool.com>
2022-03-05 20:15:04 +0300

    Reverse array

---
+++ liveReview/week08/ReverseArray.java
@@ -0,0 +1,21 @@
+package week08;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+                    // 0    1    2   3
+        int [] nums = {5 , 10 , 4, 1000, 35};
+
+        System.out.println("Before  reverse : " + Arrays.toString(nums));
+
+        for (int i = 0; i < nums.length/2; i++) {
+            int temp = nums[i]; // 5
+            nums[i]  = nums [nums.length-1 - i]; // assign 1000 to first index
+            nums[nums.length-1 - i] = temp;
+          }
+
+        System.out.println("After  reverse : " + Arrays.toString(nums));
+    }
+
+}
