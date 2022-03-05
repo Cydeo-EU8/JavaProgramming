e6c297a920839e21e9795d1163df6b04c45f97a4
OscarSah <oscar@cybertekschool.com>
2022-03-05 19:43:56 +0300

    Arrays fifth example

---
+++ liveReview/week08/SentenceSplit.java
@@ -0,0 +1,31 @@
+package week08;
+
+import java.util.Arrays;
+
+public class SentenceSplit {
+    public static void main(String[] args) {
+
+        String searchResult = "1-16 of 829 results for ipad";
+
+        // in my test cases I want to verify that my search result comes over 500
+
+       // split method turns your string into array
+
+       String[] each = searchResult.split(" ");
+
+        System.out.println(Arrays.toString(each));
+
+        String searchAmount = each[2];
+
+        System.out.println("searchAmount = " + searchAmount);
+        
+      // Break time task : capture the amount 790 with split method
+        String confirmation = "Id: 2239677\n" +
+                "Amount: 790 USD\n"+
+                "Card Number: 8548595959595\n"+
+                "Name: oscar\n"+
+                "Date: 11/10/2021";
+
+        System.out.println("confirmation = " + confirmation);
+    }
+}
