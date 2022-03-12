87576826fafe08efbaad3d89079f8746f7100a32
OscarSah <oscar@cybertekschool.com>
2022-03-12 20:33:08 +0300

    Import Class Method

---
+++ liveReview/extraPackageToShowImport/CalculatorTest2.java
@@ -0,0 +1,16 @@
+package extraPackageToShowImport;
+
+import week09.Calculator;
+
+
+
+
+public class CalculatorTest2 {
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Calculator.add(3.0,4.5) = " + Calculator.add(3.0, 4.5));
+
+    }
+}
