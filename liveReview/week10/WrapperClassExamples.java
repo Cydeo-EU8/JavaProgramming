8a4658cde1d8344fc53721ad0a0180d84f085567
OscarSah <oscar@cybertekschool.com>
2022-03-19 19:53:33 +0300

    Wrapper examples

--- liveReview/week10/WrapperClassExamples.java
+++ liveReview/week10/WrapperClassExamples.java
@@ -60,5 +60,34 @@ public class WrapperClassExamples {
 
         String strThree = "abcd1234!@#$efghi6789#$%^ABCD";
 
+        String letters = "";
+        String uppercaseLetters = "";
+        String lowerCaseLetters = "";
+        String digits = "";
+        String specialChars = "";
+
+        for (char each : strThree.toCharArray()) {   // char each : primitive
+            if(Character.isLetter(each)){
+                letters+=each;
+                if(Character.isUpperCase(each)){
+                    uppercaseLetters += each;
+                }else{
+                    lowerCaseLetters += each;
+                }
+            }else if (Character.isDigit(each)){
+                digits += each;
+            }else
+            {
+                specialChars += each;
+            }
+        }
+
+        System.out.println("letters = " + letters);
+        System.out.println("lowerCaseLetters = " + lowerCaseLetters);
+        System.out.println("uppercaseLetters = " + uppercaseLetters);
+        System.out.println("digits = " + digits);
+        System.out.println("specialChars = " + specialChars);
+
+
     }
 }
