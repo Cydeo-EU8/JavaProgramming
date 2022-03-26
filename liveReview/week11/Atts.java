3b02cb6eda09fd39e45bef2777f2efc09fca407f
OscarSah <oscar@cybertekschool.com>
2022-03-26 18:48:42 +0300

    Week 11 class Instance vs Local

--- liveReview/week11/Atts.java
+++ liveReview/week11/Atts.java
@@ -4,10 +4,10 @@ public class Atts {
 /*
 The class Atts holds a few attributes and has a method called asString. The attributes are name and color -both are strings and amount which is an int.Their visibility is public. asString returns a string showing all the Atts in a format.
  */
-   public String name,color;
+   public String name,color;  // instance variables:
    public int amount;
 
-   public String asString(){
+   public String asString(){  // instance methods
      return "name : "+name+" color : "+color+" amount : "+amount;
    }
 
