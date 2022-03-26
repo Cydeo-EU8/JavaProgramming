4910da31858bd948ff13ed59be9a25e9b1e48d5e
OscarSah <oscar@cybertekschool.com>
2022-03-26 19:25:31 +0300

    Week 11 TV class

--- liveReview/week11/TV.java
+++ liveReview/week11/TV.java
@@ -23,5 +23,29 @@ public class TV {
         }
     }
 
+    public void setVolume(int newVolume){
+        if(on&&newVolume>=1 && newVolume <=7){
+            volumeLevel = newVolume;
+        }
+    }
+
+    public void channelUp(){
+        if(on&&channel<120)
+            channel++;
+    }
+    public void channelDown(){
+        if(on&&channel>1)
+            channel--;
+    }
+    public void volumeUp(){
+        if(on&&volumeLevel<7)
+            volumeLevel++;
+    }
+    public void volumeDown(){
+        if(on&&volumeLevel>1)
+            volumeLevel--;
+    }
+
+
 
 }
