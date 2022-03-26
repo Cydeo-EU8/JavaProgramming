fd03dddda587f27137c9cf74eb291bbe9a91069e
OscarSah <oscar@cybertekschool.com>
2022-03-26 20:00:04 +0300

    Week 11 Parking Meter example

--- liveReview/week11/ParkingMeter.java
+++ liveReview/week11/ParkingMeter.java
@@ -27,6 +27,24 @@ A method named isExpired that accepts no parameters. isExpired returns a boolean
         }
         return flag;
     }
+    public void tick(){
+        if(timeLeft>0)
+            timeLeft-=1;
+    }
 
+    public boolean isExpired(){
+        return (timeLeft==0);
+    }
+    public static void main(String[] args) {
+        ParkingMeter pm = new ParkingMeter(50);
+
+        pm.add(25);
+        System.out.println("pm = " + pm.timeLeft);
+
+        while (!pm.isExpired()){
+            pm.tick();
+            System.out.println("pm.timeLeft = " + pm.timeLeft);
+        }
+    }
 
 }
