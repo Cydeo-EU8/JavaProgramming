ce84913f9d70167cc2c61149482ee50c79254e1d
OscarSah <oscar@cybertekschool.com>
2022-04-02 18:28:25 +0300

    Value Class finished

--- liveReview/week12/Value.java
+++ liveReview/week12/Value.java
@@ -10,17 +10,36 @@ a boolean method, 'wasModified' that returns true if setVal was ever called for 
 a method 'getVal' that returns an integer value as follows: if setVal has ever been called, it getVal returns the last value passed to setVal. Otherwise if the "single int parameter" constructor was used to create the object, getVal returns the value passed to that constructor. Otherwise getVal returns 0.
      */
 
-    int param = 0;
+    int param; // you can omit initialzing to zero
     boolean calledMethod = false;
+    boolean calledConstructor=false;
     public Value(){} // no parameter
     public Value(int param){ // single parameter
         this.param=param;
+        calledConstructor=true;
     }
     public void setValue(int param){ // one parameter method and is void
         this.param=param;
         calledMethod =  true;
     }
+    public boolean wasModified(){
+       return calledMethod;
+    }
 
+    public int getValue(){
+       /*
+       below code becomes redundant because it is going to return instance variable in all conditions
+       if(this.calledMethod){
+            return this.param;
+        }
+        else if(calledConstructor)
+            return this.param;
+        else{
+            return this.param;
+        }
+        */
+        return this.param;
+    }
 
 
 }
--- liveReview/week12/ValueTest.java
+++ liveReview/week12/ValueTest.java
@@ -3,9 +3,21 @@ package week12;
 public class ValueTest {
     public static void main(String[] args) {
         Value value = new Value();
-        System.out.println(value.calledMethod);
+        // print if I called set Value method
+        System.out.println(value.calledMethod); // false
+        // print the parameter without assigning anything
+        System.out.println(value.getValue()); // 0
+
         value.setValue(5);
-        System.out.println(value.calledMethod);
+        System.out.println(value.calledMethod);// true
+        System.out.println(value.getValue());// 5
+
+
+
+        Value value1 = new Value(3);
+        System.out.println(value1.calledConstructor); // true
+        System.out.println(value1.calledMethod); // false
+        System.out.println(value1.getValue()); // 3
 
     }
 }
