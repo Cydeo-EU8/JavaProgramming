ca172bb8f9ab970a2004b1f1b71a90eb87a58329
OscarSah <oscar@cybertekschool.com>
2022-04-05 20:40:56 +0300

    Avengers Arraylist methods review

--- AvengersHoursPractice/week02/ArrayListExamplesWithMethods.java
+++ AvengersHoursPractice/week02/ArrayListExamplesWithMethods.java
@@ -1,6 +1,7 @@
 package week02;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ArrayListExamplesWithMethods {
     public static void main(String[] args) {
@@ -41,6 +42,46 @@ cities.add(1,"Istanbul");
 
         // contains(): returns boolean
         System.out.println(cities.contains("Istanbul"));
+
+        // remove(index)  -- the element will be deleted at the given index
+        System.out.println(cities.remove(0)); // returns the element which is removed
+        // remove(Object) --- removes the matching object
+        System.out.println("cities removed = " + cities.remove("Istanbul"));
+        System.out.println("cities removed = " + cities.remove("Istanbul"));
+
+
+        // Bulk Operations
+        // adding array element in one shot
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        arrayList.addAll(Arrays.asList(123,234,345,456,123,234));
+
+        System.out.println("before removeAll: "+arrayList);
+        // removeAll: to remove multiple elements
+
+        arrayList.removeAll(Arrays.asList(123,345));
+        System.out.println("after removeAll: "+arrayList);
+
+        //retainAll(): if you want to retain multiple elements
+
+        ArrayList<String> names = new ArrayList<>();
+        names.addAll(Arrays.asList("Serdar","Ceyhun","Ahmet","Metin","Zehra","Mike","Serdar","Ceyhun"));
+        System.out.println("Names before RetainAll"+names);
+        names.retainAll(Arrays.asList("Serdar","Ceyhun"));
+        System.out.println("Names after RetainAll"+names);
+
+        names.addAll(Arrays.asList("Serdar","Ceyhun","Ahmet","Metin","Zehra","Mike","Serdar","Ceyhun"));
+
+        // removeIf() : removes element according to a condition
+        names.removeIf(each->each.length()>5); // each is predicate that holds place of each Arraylist element
+        System.out.println(names);
+
+        // clear()  : removes all the objects from the list
+        names.clear();
+
+        // isEmpty(): returns true if the arraylist contains NO element
+        System.out.println("is Name empty : "+names.isEmpty());
 
     }
 }
