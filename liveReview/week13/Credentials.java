eb3ba6ad55c6e9555ec8e195a3129eb9f37b6d67
OscarSah <oscar@cybertekschool.com>
2022-04-09 17:18:47 +0300

    Encapsulation Started

---
+++ liveReview/week13/Credentials.java
@@ -0,0 +1,25 @@
+package week13;
+
+public class Credentials {
+
+    private String userName = "A12345678"; // instance fields
+    private String passWord = "Cydeo123.";
+
+    // we need instance methods to read or write the data(getter and setter)
+
+    public String getUserName(){
+        if(true)// we can implement some condition to reach the data
+            return this.userName;
+        return "condition has NOT been satisfied";
+    }
+
+    public String getPassWord(){return this.passWord;}
+
+
+    public void setPassWord(String newPassword){
+        if(true)  // we require user to apply some condition
+            this.passWord=newPassword;
+        System.exit(-1);
+    }
+
+}
---
+++ liveReview/week13/Login.java
@@ -0,0 +1,16 @@
+package week13;
+
+public class Login {
+    public static void main(String[] args) {
+
+        Credentials userOne = new Credentials();
+/*        System.out.println("Username at the system for userOne is: "+userOne.userName);
+        System.out.println("Password at the system for userOne is: "+userOne.passWord);
+        userOne.userName= "differentName";
+        userOne.passWord= "differentPassword";
+        System.out.println("Username at the system for userOne is: "+userOne.userName);
+        System.out.println("Password at the system for userOne is: "+userOne.passWord);*/
+
+
+    }
+}
