ff5b4b40d1bac7da8586a088a63811c705b966f9
OscarSah <oscar@cybertekschool.com>
2022-04-12 20:45:49 +0300

    Grocery List custom class

---
+++ AvengersHoursPractice/week03/GroceryListMain.java
@@ -0,0 +1,27 @@
+package week03;
+
+public class GroceryListMain {
+    public static void main(String[] args) {
+
+        printInstructions();
+
+        boolean Continue = true;
+
+        while (Continue){
+
+        }
+    }
+
+    public static void printInstructions() {
+
+        System.out.println("\nPress");
+        System.out.println("\t 0 - To print choice options.");
+        System.out.println("\t 1 - To print the list of grocery items.");
+        System.out.println("\t 2 - To add an item to the list.");
+        System.out.println("\t 3 - To modify an item in the list.");
+        System.out.println("\t 4 - To remove an item from the list.");
+        System.out.println("\t 5 - To search an item from the list.");
+        System.out.println("\t 6 - To quit the application.");
+
+    }
+}
