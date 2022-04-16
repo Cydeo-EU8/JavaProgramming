4b75d828c9d09c0549f7612fec6aad72ae8559d6
OscarSah <oscar@cybertekschool.com>
2022-04-16 18:11:47 +0300

    Product Page

---
+++ liveReview/week14/browserPagesInheritance/ProductPage.java
@@ -0,0 +1,18 @@
+package week14.browserPagesInheritance;
+
+public class ProductPage extends AmazonHomePage {
+    public int quantity;
+
+    public ProductPage(String name, int quantity){
+        super(name);
+        this.quantity=quantity;
+    }
+
+    // need: our shopFor method is not specialized enough for this class
+    @Override   // annotation should be applicable to the method
+    public void shopFor(String product){
+        super.product=product;
+        System.out.println("Automating "+getName()); // which browser we are automating
+        System.out.println("Adding "+quantity+" amount of "+super.product+" to cart");
+    }
+}
