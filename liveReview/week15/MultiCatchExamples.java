34d45b77f2944d487cdbf209c396a84b27f3625c
OscarSah <oscar@cybertekschool.com>
2022-04-23 18:38:03 +0300

    Multi Catch statements

---
+++ liveReview/week15/MultiCatchExamples.java
@@ -0,0 +1,22 @@
+package week15;
+
+public class MultiCatchExamples {
+    public static void main(String[] args) {
+
+        // more specialized exception class statement should be first in the order
+
+        String word = "Java";  // length is 4
+
+        try {
+            System.out.println(word.substring(20));  // unchecked exception
+        }catch (StringIndexOutOfBoundsException e){
+            System.out.println(e.getClass().getSimpleName());
+            System.out.println("Reason = "+e.getMessage());
+        }catch (Throwable t){ // parent of all exceptions
+            System.out.println(t.getClass().getSimpleName());
+            System.out.println("Reason = "+t.getMessage());
+        }
+
+
+    }
+}
