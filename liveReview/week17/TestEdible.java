c3298a75cb5c41083c92be6fa1fb1120f0d7a98f
OscarSah <oscar@cybertekschool.com>
2022-05-07 21:37:24 +0300

    week 17 Interface Example

--- liveReview/week17/TestEdible.java
+++ liveReview/week17/TestEdible.java
@@ -3,6 +3,14 @@ package week17;
 public class TestEdible {
     public static void main(String[] args) {
 
+        Object[] objects = {new Tiger(), new Chicken(), new Apple()};
+
+        for (int i = 0; i < objects.length; i++) {
+            if(objects[i] instanceof Edible)
+                System.out.println(((Edible) objects[i]).howToEat());
+            if(objects[i] instanceof Animal)
+                System.out.println(((Animal) objects[i]).sound());
+        }
     }
 }
 
@@ -33,7 +41,7 @@ class Tiger extends Animal{
 
     @Override
     public String sound() {
-        return null;
+        return "Tiger : Roars";
     }
 }
 
