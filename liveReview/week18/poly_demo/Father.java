baffd8a8c876a46865c35f1d078166e7af4b0044
OscarSah <oscar@cybertekschool.com>
2022-05-14 18:44:08 +0300

    week 18 Poly Demo

--- liveReview/week18/poly_demo/Father.java
+++ liveReview/week18/poly_demo/Father.java
@@ -1,4 +1,29 @@
 package week18.poly_demo;
 
 public class Father extends Parent implements Worker{
+
+    @Override
+    public void raiseKid() {
+        System.out.println("Raising Kids, helping moms");
+    }
+
+    @Override
+    public void playWithKid() {
+        System.out.println("Playing with Kids occasionally");
+    }
+
+    @Override
+    public void feedKid() {
+        System.out.println("Providing food and supplies to home");
+    }
+
+    @Override
+    public void work(String job) {
+        System.out.println("Working as "+job);
+    }
+
+    @Override
+    public double getPaid() {
+        return 100000.0;
+    }
 }
---
+++ liveReview/week18/poly_demo/SuperManTest.java
@@ -0,0 +1,39 @@
+package week18.poly_demo;
+
+public class SuperManTest {
+    public static void main(String[] args) {
+/*
+NOTE: Reference type decides what can be accessible. Object type decides to use most specific one.
+ */
+        // polymorphism
+        // Interface/Parent Class RN = ChildClass
+
+        Parent superManOne = new Father();  // you can reach Parent Abstract Class Methodsi but CAN NOT reach Worker  Interface methods
+        superManOne.feedKid();
+        superManOne.playWithKid();
+        superManOne.raiseKid();
+
+       // superManOne.work("SDET");  // ERROR superManOne is type of Parent abstract class
+
+
+        Worker superManTwo = new Father(); // you can reach Worker  Interface methods but CAN NOT reach Parent Abstract Class Methods
+        superManTwo.getPaid();
+        superManTwo.work("SDET");
+
+        Father superManThree = new Father();
+        superManThree.feedKid();
+        superManThree.playWithKid();
+        superManThree.raiseKid();
+        superManThree.work("QA");
+        superManThree.getPaid();
+
+        boolean b1 = superManOne instanceof Worker;
+        boolean b2 = superManTwo instanceof Parent;
+        boolean b3 = superManThree instanceof Father;
+
+        System.out.println("b1 = " + b1);
+        System.out.println("b2 = " + b2);
+        System.out.println("b3 = " + b3);
+
+    }
+}
