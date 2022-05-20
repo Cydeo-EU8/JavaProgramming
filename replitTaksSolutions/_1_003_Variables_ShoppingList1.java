af76b7e6a65fc98105f65b1787578ae7008ff710
OscarSah <oscar@cybertekschool.com>
2022-05-20 07:50:12 +0300

    week 20 codes added

--- replitTaksSolutions/_1_003_Variables_ShoppingList1.java
+++ replitTaksSolutions/_1_003_Variables_ShoppingList1.java
@@ -17,6 +17,31 @@
 import java.util.Scanner;
 
 public class _1_003_Variables_ShoppingList1 {
+    /*
+    In this assignment you will write a program to create a shopping list and prices.
+
+You will use Scanner object and ask user to enter 3 items followed by its price and you will calculate total price and show as a report.
+
+-Declare 4 String variables item1, item2, item3, report.
+
+-Declare double variables price1, price2, price3, totalPrice
+
+Example:
+
+Enter Item1 and its price:
+Tomatoes
+5.5
+"Enter Item2 and its price:
+Cheese
+3.5
+Enter Item3 and its price:
+Apples
+6.3
+
+Output:
+Item1: Tomatoes Price: 5.5, Item2: Cheese Price: 3.5, Item3: Apples Price: 6.3
+Total price: 15.3
+     */
     public static void main(String[] args) {
         //WRITE YOUR CODE HERE:
         String item1,item2,item3;
