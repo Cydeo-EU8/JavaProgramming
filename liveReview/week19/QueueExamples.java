0a6fe3e0e40f52b33f17343e20d4e0e7c866ded5
OscarSah <oscar@cybertekschool.com>
2022-05-21 19:20:45 +0300

    Collections Example

--- liveReview/week19/QueueExamples.java
+++ liveReview/week19/QueueExamples.java
@@ -1,14 +1,31 @@
 package week19;
 
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class QueueExamples {
     public static void main(String[] args) {
 
+        Queue<String> cities = new ArrayDeque<>();
 
-        Queue<String> listOne = new LinkedList<>();
+        cities.add("Ankara");
+        cities.addAll(Arrays.asList("NewYork","Athens","Washington","Brussels","Paris"));
+
+        Queue<String> citiesLinkedList = new LinkedList<>();
+        citiesLinkedList.addAll(cities);
+
+        // These data structures works as FIFO: First In First Out
+
+        for (int i = 0; i < cities.size(); ) {
+            // FIFO
+            System.out.println(cities.poll()); // brings the head of the list and deletes
+          //  System.out.println(cities.peek()); // brings the head and does not delete
+
+        }
+        System.out.println("cities.size() = " + cities.size());
 
     }
 }
