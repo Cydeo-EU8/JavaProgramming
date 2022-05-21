e8e148246722b0f4ace39efa2dd0c9204f7185a0
OscarSah <oscar@cybertekschool.com>
2022-05-21 18:37:05 +0300

    Collections Example

---
+++ liveReview/week19/CollectionsExamples.java
@@ -0,0 +1,58 @@
+package week19;
+
+import java.util.*;
+
+public class CollectionsExamples {
+    public static void main(String[] args) {
+
+        List<String> listOne = new ArrayList<>(); // polymorphic way
+        ArrayList<String> arrayList = new ArrayList<>();// not polymorphic way
+
+        List<String> listTwo = new Stack<>();
+
+        listOne.add("Mike");
+        listOne.add("Oscar");
+        listOne.addAll(Arrays.asList("Selena","Cemal","Nihal","Selena","Oscar"));
+        System.out.println("listOne = " + listOne);
+
+        listTwo.addAll(listOne);
+        System.out.println("listTwo = " + listTwo);
+
+        /*
+        Stack Methods
+        peek() ---> brings you the element which last LIFO: Last in First Out
+        pop() ---> brings you the element which last LIFO: Last in First Out and DELETES the element
+        push() ---> similar to add method
+         */
+
+        String str = ((Stack<String>) listTwo).peek(); // since it is created in polymormphic way I have to cast to STack
+        System.out.println("str = " + str);
+
+        str = ((Stack<String>) listTwo).pop(); // since it is created in polymormphic way I have to cast to STack
+        System.out.println("str = " + str);
+
+        System.out.println("listTwo after pop method = " + listTwo);
+
+        ((Stack<String>) listTwo).push("Oscar");
+        System.out.println("listTwo after push method = " + listTwo);
+
+
+        // Collections Utility class comes with useful methods: such as sorting
+         Collections.sort(listTwo);    //comparable interface does not work with null values
+       System.out.println("listTwo after sorting= " + listTwo);
+       // listOne.add(null); comparable interface does not work with null values
+        Collections.sort(listOne);
+        System.out.println("listOne = " + listOne);
+
+
+        List<Integer> listThree = new Stack<>();
+        listThree.addAll(Arrays.asList(45,55,2,89,34,1000,-56,564,987));
+        System.out.println("listThree before sorting= " + listThree);
+        Collections.sort(listThree);
+        System.out.println("listThree after sorting = " + listThree);
+
+// ***** Likewise TreeSet does not accept null value since internally using Sorting
+ // Any collection framework which has some type sorting  -----> DOES NOT ACCEPT NULL Values
+
+    }
+}
