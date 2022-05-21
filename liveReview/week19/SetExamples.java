ac088d12cf86ca0dfa1eca4c0be76add17a77238
OscarSah <oscar@cybertekschool.com>
2022-05-21 19:41:51 +0300

    Collections Example

--- liveReview/week19/SetExamples.java
+++ liveReview/week19/SetExamples.java
@@ -7,16 +7,29 @@ public class SetExamples {
 
         // Does NOT allow duplicates: helps you to implement some coding algorithms
 
-        String str = "aaaabbbcccccddddeeee"; // str to char array
+        String str = "zzzzoooooxxxxxxxaaaabbbcccccddddeeee"; // str to char array
         List<String> listOne = new ArrayList<>();
         listOne.addAll(Arrays.asList(str.split("")));
         System.out.println("listOne = " + listOne);
 
-        Set<String> stringSet = new LinkedHashSet<>(); // to keep my insertion order
-        stringSet.addAll(listOne); // What happens at this line, duplicates are removed
-        System.out.println("stringSet = " + stringSet);
-        stringSet.add(null);
-        System.out.println("stringSet after adding null = " + stringSet);
+        Set<String> linkedHashSet = new LinkedHashSet<>(); // to keep my insertion order
+        linkedHashSet.addAll(listOne); // What happens at this line, duplicates are removed
+        System.out.println("stringSet = " + linkedHashSet);
+        linkedHashSet.add(null);
+        System.out.println("stringSet after adding null = " + linkedHashSet);
+
+        Set<String> hashSet = new HashSet<>(); // if we don't need any kind of order we can go ahead with HashSet, we can put one null value
+        hashSet.addAll(listOne);
+        System.out.println("stringSet1 = " + hashSet);
+        hashSet.add("g");
+        hashSet.add("null");
+        System.out.println("stringSet1 = " + hashSet);
+
+        Set<String> treeSet = new TreeSet<>(); // orders the list: sorting ==>> according to the asci table
+        treeSet.addAll(listOne);
+      //  treeSet.add(null); // Exception in thread "main" java.lang.NullPointerException
+        System.out.println("stringSet2 = " + treeSet);
+
 
     }
 }
