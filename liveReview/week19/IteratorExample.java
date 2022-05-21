293027ffde6e14769f01dbd5832dc078f32ab7d5
OscarSah <oscar@cybertekschool.com>
2022-05-21 19:59:26 +0300

    Collections Example

--- liveReview/week19/IteratorExample.java
+++ liveReview/week19/IteratorExample.java
@@ -35,5 +35,13 @@ public class IteratorExample {
         }
         System.out.println("nums = " + nums.size());
         // Iterator is another way of looping inside your collection
+
+
+        List<Integer> nums2 = new LinkedList<>();
+        nums2.addAll(Arrays.asList(34,67,33,12,11,65,100));
+
+        for (Integer integer : nums2) { // Exception in thread "main" java.util.ConcurrentModificationException
+            nums2.remove(integer);
+        }
     }
 }
