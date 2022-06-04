dba3d1901c6198f75983991484325a0edda919b4
OscarSah <oscar@cybertekschool.com>
2022-06-04 18:16:59 +0300

    Employee Class for Map Example

--- liveReview/week20/MapExampleWithClassObject.java
+++ liveReview/week20/MapExampleWithClassObject.java
@@ -12,7 +12,18 @@ public class MapExampleWithClassObject {
 
     cydeo.put("empOne",empOne);
 
+        Employee empTwo = new Employee();
+        empTwo.setInfo("Neena","Kochar","AD_VP",17000,90);
 
+        cydeo.put("empTwo",empTwo);
+
+        Employee empThree = new Employee();
+        empTwo.setInfo("Lex","De Haan","AD_VP",17000,90);
+
+        cydeo.put("empThree",empThree);
+
+
+      //  System.out.println("cydeo = " + cydeo);
 
 
     }
